b2b3429335f9e7d204c1b0e26f39047404bf2c7b
somecodehere <65556294+somecodehere@users.noreply.github.com>
2020-05-29 00:55:37 +0300

    Движок учебника

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ReactComponent as HomeIcon } from './home.svg';
+
+import './style.css';
+
+
+const PARTS_COMPONENTS = [  ];
+
+const Link = ({ number }) => <a href={`/${number}`}>Часть {number}</a>;
+const Page = ({ children }) => <div className="Page">{children}</div>;
+
+const HomeLink = ({ pageName }) =>
+  <div className="HomeLink">
+    <a href="/">
+      <HomeIcon />
+    </a>
+    <p>{pageName}</p>
+    <h1>React + Redux</h1>
+  </div>
+
+const PartsList = () => {
+  const total_parts = PARTS_COMPONENTS.length;
+  const list = Array(total_parts).fill().map((el, i) => <Link key={i} number={i} />);
+  return <div className="PartsList">{list}</div>;
+};
+
+function getPartFromUrl() {
+  const { pathname } = window.location;
+
+  const [ part ] = pathname.split('/').filter(item => item !== '');
+  const part_index = parseInt(part);
+
+  return part_index;
+}
+
+function getPageComponent() {
+
+    const part_index = getPartFromUrl();
+
+    if (isNaN(part_index)) {
+      return PartsList;
+    }
+
+    const PartComponent = PARTS_COMPONENTS[part_index];
+
+    return PartComponent;
+    
+}
+
+function getPageName() {
+
+  const part_index = getPartFromUrl();
+
+  if (!isNaN(part_index)) {
+    return `Часть ${part_index}`;
+  }
+
+  return 'Список глав';
+
+}
+
+const CurrentComponent = getPageComponent();
+const pageName = getPageName();
+
+ReactDOM.render(
+  <React.StrictMode>
+      <React.Suspense fallback={<div></div>}>
+        <HomeLink pageName={pageName} />
+        <Page>
+          <CurrentComponent />
+        </Page>
+      </React.Suspense>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
